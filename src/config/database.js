8f5b4598aaa9dc2dc56c2b4d1b5ed1513460362a
wagnerk78 <wagnerkroiss@gmail.com>
2023-03-27 20:39:49 -0300

    Third day

---
+++ src/config/database.js
@@ -0,0 +1,13 @@
+/* eslint-disable prettier/prettier */
+module.exports = {
+    dialect: 'postgres',
+    host: 'localhost',
+    username: 'postgres',
+    password: '123456',
+    database: 'code-burguer',
+    define: {
+        timespamps: true,
+        underscored: true,
+        underscoredAll: true
+    },
+}
