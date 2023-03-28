2995830a5871e3a9d2f652ace060b5d266caee9c
wagnerk78 <wagnerkroiss@gmail.com>
2023-03-28 19:53:11 -0300

    Third day

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,18 @@
 import { Router } from 'express'
+import { v4 } from 'uuid'
+import User from './app/models/User'
 
 const routes = new Router()
 
-routes.get('/', (request, response) => {
-  return response.json({ mensage: 'Hello World' })
+routes.get('/', async (request, response) => {
+  const user = await User.create({
+    id: v4(),
+    name: 'Wagner',
+    email: 'wagnerkr78@gmail.com',
+    password_hash: '123456',
+    admin: true,
+  })
+  return response.json(user)
 })
 
 export default routes
