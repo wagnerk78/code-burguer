bee7faf234734dc1507b83fc4f51fb9d48aed21f
wagnerk78 <wagnerkroiss@gmail.com>
2023-04-10 21:23:21 -0300

    10th April

---
+++ src/app/models/Category.js
@@ -0,0 +1,17 @@
+/* eslint-disable prettier/prettier */
+import Sequelize, { Model } from 'sequelize'
+
+class Category extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        name: Sequelize.STRING,
+      },
+      {
+        sequelize,
+      }
+    )
+  }
+}
+
+export default Category
